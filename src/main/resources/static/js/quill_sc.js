bbe301fdcc2879d2a1b8a5018b1e23953218dcce
DESKTOP-IM6C1MF\Sandiaz <archawatip_seeneha@kkumail.com>
2020-11-11 23:30:45 +0700

    DH-DEK HERE 1.0 NOW RELEASE -Improve article system -Upgrade Bootstrap 3 to Bootstrap 4 -Added Color

--- src/main/resources/static/js/quill_sc.js
+++ src/main/resources/static/js/quill_sc.js
@@ -1,25 +1,25 @@
-var quill = new Quill('#editor-container', {
+const quill = new Quill('#editor-container', {
   modules: {
     'toolbar': [
-      [{ 'font': [] }, { 'size': [] }],
-      [ 'bold', 'italic', 'underline', 'strike' ],
-      [{ 'color': [] }, { 'background': [] }],
-      [{ 'script': 'super' }, { 'script': 'sub' }],
-      [{ 'header': '1' }, { 'header': '2' }, 'blockquote', 'code-block' ],
-      [{ 'list': 'ordered' }, { 'list': 'bullet'}, { 'indent': '-1' }, { 'indent': '+1' }],
-      [ 'direction', { 'align': [] }],
-      [ 'link', 'image', 'video', 'formula' ],
-      [ 'clean' ]
-        ]
+      [{'font': []}, {'size': []}],
+      ['bold', 'italic', 'underline', 'strike'],
+      [{'color': []}, {'background': []}],
+      [{'script': 'super'}, {'script': 'sub'}],
+      [{'header': '1'}, {'header': '2'}, 'blockquote', 'code-block'],
+      [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+      ['direction', {'align': []}],
+      ['link', 'image', 'video', 'formula'],
+      ['clean']
+    ]
   },
-  placeholder: 'Compose an epic...',
+  placeholder: 'อยากเขียนหยังกะเขียนโลด',
   theme: 'snow'
 });
 
-var form = document.querySelector('form');
+const form = document.querySelector('form');
 form.onsubmit = function() {
   // Populate hidden form on submit
-  var about = document.querySelector('input[name=content]');
+  const about = document.querySelector('input[name=content]');
   about.value = quill.root.innerHTML;
 
   // No back end to actually submit to!
